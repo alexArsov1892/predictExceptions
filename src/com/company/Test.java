96f8283bdf1551d8ad394252125dd415e2a28f4f
alexArsov1892 <59705322+alexArsov1892@users.noreply.github.com>
2020-01-22 23:38:33 +0200

    Initial commit: Predict Exceptions

---
+++ src/com/company/Test.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Test {
+    private String str = "a";
+
+    void A() {
+        try {
+            str += "b";
+            B();
+        } catch (Exception e) {
+            str += "c";
+        }
+    }
+
+    void B() throws Exception {
+        try {
+            str += "d";
+            C();
+        } catch (Exception e) {
+            throw new Exception();
+        } finally {
+            str += "e";
+        }
+
+        str += "f";
+
+    }
+
+    void C() throws Exception {
+        throw new Exception();
+    }
+
+    void display() {
+        System.out.println(str);
+    }
+}
